007d62ac1ed449fa55a4bbbb5188a68a96ab9fef
Jared <kuil5230@gmail.com>
2017-12-24 00:09:35 +0800

    first commit

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require("babel-runtime/helpers/createClass");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require("babel-runtime/helpers/inherits");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Error = function (_React$Component) {
+  (0, _inherits3.default)(Error, _React$Component);
+
+  function Error() {
+    (0, _classCallCheck3.default)(this, Error);
+
+    return (0, _possibleConstructorReturn3.default)(this, (Error.__proto__ || (0, _getPrototypeOf2.default)(Error)).apply(this, arguments));
+  }
+
+  (0, _createClass3.default)(Error, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement("p", null, this.props.statusCode ? "An error " + this.props.statusCode + " occurred on server" : "An error occurred on client");
+    }
+  }], [{
+    key: "getInitialProps",
+    value: function getInitialProps(_ref) {
+      var res = _ref.res,
+          err = _ref.err;
+
+      var statusCode = res ? res.statusCode : err ? err.statusCode : null;
+      return { statusCode: statusCode };
+    }
+  }]);
+
+  return Error;
+}(_react2.default.Component);
+
+exports.default = Error;
